49e97334adac49a88e15705bbe27026480401a3e
edwinB-TAM <57118256+edwinB-TAM@users.noreply.github.com>
2023-02-20 06:53:58 -0800

    Working iOS version

--- tam-tool.js
+++ tam-tool.js
@@ -1,17 +1,17 @@
-fs = require('fs')
+const fs = require('fs')
 const qfgets = require('qfgets');
 const { spawn } = require('child_process');
 const integrationHelperParser = require('./integrationHelperParser');
 const date = new Date();
 const timestamp = date.getTime();
 //Android command line
-const subprocess = spawn('adb', ['logcat', '-s', 'IntegrationHelper'], {detached: true});
+// const subprocess = spawn('adb', ['logcat', '-s', 'IntegrationHelper'], {detached: true});
 //// TODO: iOS command line
 // const subprocess = spawn('cfgutil', ['syslog','grep','-r','IntegrationHelper'], {detached: true});
 
 //iOS
-// const subprocess = spawn('cfgutil',['syslog']);
-// const subprocess = child_process.exec(grep', ['-r', 'IntegrationHelper'])
+var args = ['-c','cfgutil syslog |grep -r IntegrationHelper']
+const subprocess = spawn('sh', args, {detached: true});
 const logging = fs.createWriteStream(`test-${timestamp}.log`, {flags: 'a'});
 
 //TODO kill process
@@ -41,6 +41,6 @@ function closeUserLogs(args){
   integrationHelperParser.integrationHelperJson(path)
   process.kill(-subprocess.pid);
 }
-// console.log("before");
+console.log("Please launch the application now");
 setTimeout(getUserLogs,15, 'close');
 // console.log("after");
