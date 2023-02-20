879c9fd3ff2fccec8565ee3f60ecf560c401539b
edwinB-TAM <57118256+edwinB-TAM@users.noreply.github.com>
2023-02-20 06:54:49 -0800

    Working iOS version

--- integrationHelperParser.js
+++ integrationHelperParser.js
@@ -1,22 +1,32 @@
 const fs = require('fs');
+const app = require('./app');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+let result= {};
+
 //Compiled Network and Adapter matches into one regex
-const network_adapter = /\B([-]{15})\s([a-zA-Z]\w*)\s+([-]{14})\n(.+?(?:((Adapter\s)([0-9.]+\S))\s))/g;
+// const network_adapter = /\(?<=--------------- )\b[a-zA-Z]*\b(?= --------------)\n(.+?(?:((Adapter\s)([0-9.]+\S))\s))/g;
+const network_adapter = /\B([-]{15})\s([a-zA-Z]\w*)\s+([-]{14})\n.*(Adapter VERIFIED)\n.*(SDK - Version )([0-9.]+\S)( - VERIFIED)\n.*(Adapter - Version )([0-9.]+\S)/g;
 // Empty Adapter
 const adapter_missing = /Adapter - MISSING/g;
 const exit = /use this for test devices/g;
+let ironSourceAdapter_version;
 
 function integrationHelperJson(path){
   // console.log("Logging here")
   // logging = fs.readFileSync(path).toString();
-  const result = Array.from(path.matchAll(network_adapter), m => {
-      let res = {"Network": m[2]}
-      if (undefined !== m[7]) {
-          res["Adapter"]=m[7];
-      }
-      return res;
-  });
-  console.log(result);
-  fs.writeFileSync("parsedLogs"+Date.now(),JSON.stringify(result));
+  const matches = path.matchAll(network_adapter);
+for (const m of matches) {
+    let network = m[2];
+    let adapter = m[9];
+    result[network] = { adapter };
+    if (network === "IronSource") {
+        ironSourceAdapter_version = adapter;
+    }
+}
+console.log(result);
+app.scrape(result, ironSourceAdapter_version);
 }
 
 exports.integrationHelperJson = integrationHelperJson;
